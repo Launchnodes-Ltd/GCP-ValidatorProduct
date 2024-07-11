9aa4b0bf16dede1be7d5726d6e3e6d62ea5ee456
gauravplaunchnodes <169021397+gauravplaunchnodes@users.noreply.github.com>
2024-07-11 10:20:12 +0530

    Update gcp-setupWallet.sh

--- gcp-setupWallet.sh
+++ gcp-setupWallet.sh
@@ -6,6 +6,8 @@ function validate_password() {
         read -s walletpassword
         echo "echo username through which you are running this command or have done SSH"
         read  username
+        echo "echo your wallet address"
+        read  walletaddress
         if [[ ${#walletpassword} -ge 8 && "$walletpassword" == *[A-Z]* && "$walletpassword" == *[a-z]* && "$walletpassword" == *[0-9]* ]]; then
         
             if [[ $walletpassword =~ ['!@#$%^&*()_+'] ]]; then
@@ -26,12 +28,14 @@ function validate_password() {
                 
                 sudo docker run -it -v /home/"$username"/Eth2Validators/prysm-wallet-v2:/wallet -v /home/"$username"/password:/password --network="host" gcr.io/prysmaticlabs/prysm/validator:v5.0.3 wallet create --wallet-dir=/wallet --accept-terms-of-use --wallet-password-file=/password/pass.txt
                 
-		        sudo docker run -it -v /home/"$username"/validator_keys:/keys -v /home/"$username"/Eth2Validators/prysm-wallet-v2:/wallet -v /home/"$username"/Eth2:/validatorDB -v /home/"$username"/password:/password --name AccountImport gcr.io/prysmaticlabs/prysm/validator:v5.0.3 --beacon-rpc-provider=34.123.232.83:4000 --attest-timely --enable-builder --datadir=/validatorDB accounts import --keys-dir=/keys --wallet-dir=/wallet --accept-terms-of-use --wallet-password-file=/password/pass.txt
+	        sudo docker run -it -v /home/"$username"/validator_keys:/keys -v /home/"$username"/Eth2Validators/prysm-wallet-v2:/wallet -v /home/"$username"/Eth2:/validatorDB -v /home/"$username"/password:/password --name AccountImport gcr.io/prysmaticlabs/prysm/validator:v5.0.3 --beacon-rpc-provider=34.123.232.83:4000 --attest-timely --enable-builder --datadir=/validatorDB accounts import --keys-dir=/keys --wallet-dir=/wallet --accept-terms-of-use --wallet-password-file=/password/pass.txt
                 sudo chown -R "$username":"$username" /home/"$username"/Eth2 /home/"$username"/Eth2Validators
                 sudo chmod 700 /home/"$username"/Eth2 /home/"$username"/Eth2Validators
 
+  		docker run -d -it -v /home/"$username"/Eth2Validators/prysm-wallet-v2:/wallet -v /home/"$username"/Eth2:/validatorDB --network="host" --name validator gcr.io/prysmaticlabs/prysm/validator:v5.0.2 --beacon-rpc-provider=95.179.226.159:4000 --holesky  --wallet-dir=/wallet --datadir=/validatorDB --suggested-fee-recipient="$walletaddress"
+
                 ################# New Docker pull command ###########################################
-                #sudo docker pull us-central1-docker.pkg.dev/launchnodesltd-public/validator-metering/latest:latest
+                sudo docker pull us-central1-docker.pkg.dev/launchnodesltd-public/validator-metering/latest:latest
                 
                 return
             else
