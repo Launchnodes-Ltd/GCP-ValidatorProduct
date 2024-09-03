027cb77c5154ea0410fc6de929a4d515febeee7c
gauravplaunchnodes <169021397+gauravplaunchnodes@users.noreply.github.com>
2024-09-03 10:47:51 +0530

    Update gcp-setupWallet.sh

--- gcp-setupWallet.sh
+++ gcp-setupWallet.sh
@@ -40,7 +40,7 @@ function validate_password() {
   		echo "Running validator"
     		#echo  docker run -d -it -v /home/"$username"/Eth2Validators/prysm-wallet-v2:/wallet -v /home/"$username"/Eth2:/validatorDB --network="host" --name validator gcr.io/prysmaticlabs/prysm/validator:v5.0.2 --beacon-rpc-provider=34.123.232.83:4000 --mainnet  --wallet-dir=/wallet --datadir=/validatorDB --suggested-fee-recipient="$walletaddress"
 
-		sudo docker rm validator
+		sudo docker rm -f validator
 
     		sudo docker run -d -it -v /home/"$username"/Eth2Validators/prysm-wallet-v2:/wallet -v /home/"$username"/Eth2:/validatorDB -v /home/"$username"/password:/password --network="host" --name validator gcr.io/prysmaticlabs/prysm/validator:v5.0.2 --beacon-rpc-provider=34.123.232.83:4000 --mainnet  --wallet-dir=/wallet --attest-timely --enable-builder --accept-terms-of-use --wallet-password-file=/password/pass.txt --datadir=/validatorDB --suggested-fee-recipient="$walletaddress"
 
